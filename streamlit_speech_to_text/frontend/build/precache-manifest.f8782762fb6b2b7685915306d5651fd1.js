68f0b8e275f0db165a3b925d41d0b849110e975a
Joan_George_01 <jgtharchius@raisetech.io>
2023-12-27 10:34:34 +0530

    build file

---
+++ streamlit_speech_to_text/frontend/build/precache-manifest.f8782762fb6b2b7685915306d5651fd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b726935d4c5fd2066c6651e697c2da3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0f0d76850f2fc7be26cb",
+    "url": "./static/js/2.d186d055.chunk.js"
+  },
+  {
+    "revision": "d63d4b2625aba02e8f0d93c7089d8c90",
+    "url": "./static/js/2.d186d055.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1d8671c3595d89f900",
+    "url": "./static/js/main.7bce25f7.chunk.js"
+  },
+  {
+    "revision": "63841914880063de1403",
+    "url": "./static/js/runtime-main.190cdfe0.js"
+  }
+]);
